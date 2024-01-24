c77161135aaff97fafcf035c984861c887b44fac
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2024-01-24 12:24:09 +0000

    Collection filtering script new reports missing object ids gh-46

--- tools/filterCollection.js
+++ tools/filterCollection.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 let srcPath = process.argv.pop()
 if (!srcPath.endsWith('.json')) {
-    console.log('First argument should be the path to a full DTS collection (.json file)')
+    console.log('First argument should be the path of a json file with an array of object ids.')
     process.exit(1)
 }
 
-// let srcPath = '../app/data/2023-08/inscriptions.json'
+const shortListPath = '../app/data/2023-08/inscriptions.json'
 const fullCollectionPath = '../app/data/dts/api/collections.json'
-let dstPath = path.dirname(srcPath) + '/' + 'collection.json'
+let filteredCollectionPath = path.dirname(srcPath) + '/' + 'collection.json'
 
 async function readJsonFile(path) {
     let content = fs.readFileSync(path);
@@ -22,17 +22,32 @@ async function writeJsonFile(path, content) {
 
 async function filter() {
     // 1. read the list of inscription ids
-    let inscriptions = await readJsonFile('../app/data/2023-08/inscriptions.json')
+    let shortList = await readJsonFile(shortListPath)
     // 2. read the collection
-    let collection = await readJsonFile(fullCollectionPath)
+    let fullCollection = await readJsonFile(fullCollectionPath)
+    let filteredCollection = await readJsonFile(fullCollectionPath)
 
     // 3. filter the collection by the list of inscriptions
-    collection.member = collection.member.filter(m => inscriptions.includes(m?.title))
+    filteredCollection.member = fullCollection.member.filter(m => shortList.includes(m?.title))
 
     // 4. write the filtered collection
-    await writeJsonFile(dstPath, collection)
+    await writeJsonFile(filteredCollectionPath, filteredCollection)
+
+    // 5. report ids that were not found
+    let missingids = []
+    let allids = filteredCollection.member.map(m => m?.title)
+    for (iid of shortList) {
+        if (!allids.includes(iid)) {
+            console.log(`WARNING: object id (${iid}) not found in the base collection.`)
+            missingids.push(iid)
+        }
+    }
     
-    console.log(`WRITTEN ${collection.member.length} inscriptions into ${dstPath}`)
+    console.log(`TASK    filter ${shortList.length} inscriptions (${shortListPath}) from ${fullCollection.member.length} (${fullCollectionPath})`)
+    console.log(`WRITTEN ${filteredCollection.member.length} inscriptions into ${filteredCollectionPath}`)
+    if (missingids.length) {
+        console.log(`WARNING: ${missingids} were not found in the full collection file.`)
+    }
 }
 
 filter()

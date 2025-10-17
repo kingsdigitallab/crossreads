04861fa18e6e373543a07ae86de87a93e04f8dbb
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2025-10-17 16:15:17 +0100

    doc: settings tab now shows all the links to external resources; gh-119

--- app/assets/settings.mjs
+++ app/assets/settings.mjs
@@ -1,4 +1,4 @@
-import { utils } from "../utils.mjs";
+import { utils, SETTINGS, FILE_PATHS } from "../utils.mjs";
 import { AnyFileSystem } from "../any-file-system.mjs";
 import { createApp } from "../node_modules/vue/dist/vue.esm-browser.js";
 
@@ -10,6 +10,36 @@ createApp({
         gtoken: window.localStorage.getItem('gtoken') || '',
       },
       afs: new AnyFileSystem(),
+      links: [
+        {
+          'name': 'Annotations files',
+          'url': `${SETTINGS.GITHUB_REPO_PATH}/${FILE_PATHS.ANNOTATIONS}`,
+        },
+        {
+          'name': 'Other data files (definitions, variant-rules, search index)',
+          'url': SETTINGS.GITHUB_REPO_URL,
+        },
+        {
+          'name': 'Image server',
+          'url': SETTINGS.IIIF_SERVER_BASE.replace(/\?.*$/, ""),
+        },
+        {
+          'name': 'DTS Collection',
+          'url': SETTINGS.DTS_COLLECTION,
+        },
+        {
+          'name': 'CROSSREADS portal',
+          'url': SETTINGS.CROSSREADS_PORTAL_ROOT,
+        },
+        {
+          'name': 'Annotating site',
+          'url': SETTINGS.ANNOTATING_SITE_ROOT,
+        },
+        {
+          'name': 'Inscription web pages',
+          'url': SETTINGS.CORPUS_BUILDING_INSCRIPTION_URL.replace('{docId}', 'ISic000085'),
+        },
+      ]
     }
   },
   async mounted() {
